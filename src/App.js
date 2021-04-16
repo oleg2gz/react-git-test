7dc338546e9d8939504f320abf26df9e32483cc0
Oleg Kravchenko <kitaist@kitaist.com>
2021-04-17 00:54:04 +0800

    Feature/Add class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import CounterClass from './CounterClass'
+
 function App() {
-  return null
+  return (
+    <>
+      <h3>Class Component</h3>
+      <CounterClass initialCount={0} />
+    </>
+  )
 }
 
 export default App
